6dc07b4b37781e4a93bdbccf4e00cc5f4c7b5a1c
Christopher Richardson <christopherrichardson518@gmail.com>
2023-10-14 14:46:51 -0400

    JEngine first commit test

---
+++ JEngine/src/com/tripod/main/Log.java
@@ -0,0 +1,19 @@
+package com.tripod.main;
+
+import java.time.LocalDate;
+
+public class Log {
+	
+	private final static int versionNumber = 1;
+	private final static LocalDate date = LocalDate.now();
+	
+	// LOG MESSAGES
+	public static final String VERSION = "JEngine Version "
+											+ versionNumber + " : "
+											+ date;
+	
+	public static void code (String code) {
+		System.out.println(code);
+	}
+
+}
---
+++ JEngine/src/com/tripod/main/Main.java
@@ -0,0 +1,11 @@
+package com.tripod.main;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Log.code(Log.VERSION);
+	}
+
+}
